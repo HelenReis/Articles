c1ea5f5fe8cea1446a1195597d1c29dc947910f3
Helen Reis <heel.alves6@gmail.com>
2020-10-31 11:35:28 -0300

    Melhorias no código node.js

--- Software-Architecture-NodeJs/exercicio-node/index.js
+++ Software-Architecture-NodeJs/exercicio-node/index.js
@@ -1,6 +1,6 @@
 const operacoesMatematicas = require("./operacoes/operacoes-matematicas");
 
-console.log("a) " + operacoesMatematicas.adicao(1, 1));
+console.log("a) " + operacoesMatematicas.adicao("1", 1));
 console.log("b) " + operacoesMatematicas.adicao(-123, 123123));
 console.log("c) " + operacoesMatematicas.multiplicacao(8, 0));
 console.log("d) " + operacoesMatematicas.multiplicacao(1239123, 12313));
--- Software-Architecture-NodeJs/exercicio-node/operacoes/operacoes-matematicas.js
+++ Software-Architecture-NodeJs/exercicio-node/operacoes/operacoes-matematicas.js
@@ -1,30 +1,31 @@
 function subtracao(n1, n2) {
-  let result = parseInt(n1 - n2);
+  let result = parseInt(parseInt(n1) - parseInt(n2));
+  if (isNaN(result)) return Error("Invalid Operation.");
 
   return result;
 }
 
 function adicao(n1, n2) {
-  let result = parseInt(n1 + n2);
-
-  if (isNaN(result));
-  {
-    return Error("Invalid Operation.");
-  }
+  let result = parseInt(parseInt(n1) + parseInt(n2));
+  if (isNaN(result)) return Error("Invalid Operation.");
 
   return result;
 }
 
 function multiplicacao(n1, n2) {
-  return n1 * n2;
+  let result = parseInt(parseInt(n1) * parseInt(n2));
+  if (isNaN(result)) return Error("Invalid Operation.");
+
+  return result;
 }
 
 function divisao(n1, n2) {
-  if (n2 == 0) {
-    return "Não é possível dividir por zero";
-  }
+  if (n2 == 0) return "Não é possível dividir por zero";
 
-  return n1 / n2;
+  let result = parseInt(parseInt(n1) / parseInt(n2));
+  if (isNaN(result)) return Error("Invalid Operation.");
+
+  return result;
 }
 
 module.exports = {
